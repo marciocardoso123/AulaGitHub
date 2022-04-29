ab2bd6735f831d5d81ab0feece7b4d7c9043e107
mmc <marciodalu@gmail.com>
2022-04-29 18:56:58 -0300

    Atualização

---
+++ src/main/java/Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+    public static void main(String[] args) {
+        int num =10;
+        int num2 = 20;
+        int soma;
+
+        soma = num + num2;
+        System.out.println(soma);
+
+    }
+
+}
